a8d210991e5447335b12bb364036b212ee8b0645
ephraim <ephlind@gmail.com>
2024-08-17 11:59:21 -0700

    fix for domains

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import dog1Image from '/dog1.jph';
+import dog1Image from '/dog1.jpg';
+import dog2Image from '/dog2.jpg';
+import dog3Image from '/dog3.jpg';
 import catsupImage from '/catsup.png';
 import './App.css'
 
 function App() {
-    if (window.location.hostname === 'wong4judge.com'){
+    if (window.location.hostname === 'wongforjudge.com') {
+        document.title = "Dog"
         return (
             <>
                 <img src={dog1Image} className="logo" alt="Ketchup"/>
+                <h1>Ketchup</h1>
+            </>
+        )
+    }
+    if (window.location.hostname === 'wong4judge.com') {
+        document.title = "Dog Dog"
+        return (
+            <>
+                <img src={dog2Image} className="logo" alt="Ketchup"/>
+                <h1>Ketchup</h1>
+            </>
+        )
+    }
+    if (window.location.hostname === 'andrewwong4judge.com') {
+        document.title = "Dog Dog Dog"
+        return (
+            <>
+                <img src={dog3Image} className="logo" alt="Ketchup"/>
                 <h1>Ketchup</h1>
             </>
         )
