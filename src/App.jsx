07eb5dc6516a2a7cc5147088ff7e470fdd59e731
ephraim <ephlind@gmail.com>
2024-08-17 11:55:59 -0700

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import catsupImage from '/catsup.png'
+import dog1Image from '/dog1.jph';
+import catsupImage from '/catsup.png';
 import './App.css'
 
 function App() {
-
+    if (window.location.hostname === 'wong4judge.com'){
+        return (
+            <>
+                <img src={dog1Image} className="logo" alt="Ketchup"/>
+                <h1>Ketchup</h1>
+            </>
+        )
+    }
     return (
         <>
             <img src={catsupImage} className="logo" alt="Ketchup"/>
