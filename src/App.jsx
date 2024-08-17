cad43c63b471e99331505a41011e68b123bc216f
ephraim <ephlind@gmail.com>
2024-08-17 10:55:46 -0700

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import './App.css'
 
 function App() {
 
-  return (
-    <>
-      <div>
-          <img src={catsupImage} className="logo" alt="Ketchup" />
-      </div>
-      <h1>Ketchup</h1>
-    </>
-  )
+    return (
+        <>
+            <img src={catsupImage} className="logo" alt="Ketchup"/>
+            <h1>Ketchup</h1>
+        </>
+    )
 }
 
 export default App
