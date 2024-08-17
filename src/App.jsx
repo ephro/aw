b17a813d61ec7f174e90653284f14e0e7559db92
ephraim <ephlind@gmail.com>
2024-08-17 09:50:56 -0700

    fire commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import catsupImage from '/catsup.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <img src={catsupImage} className="logo" alt="Ketchup" />
+      </div>
+      <h1>Ketchup</h1>
+    </>
+  )
+}
+
+export default App
