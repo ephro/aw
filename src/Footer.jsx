e2cf27c1bd5f522f8e62293d4836ea33d2750193
ephraim <ephlind@gmail.com>
2024-08-17 19:50:59 -0700

    get rid of last |

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
             <a href="https://andrewwong4judge.com">andrewwong4judge.com</a> |&nbsp;
             <a href="https://andrewwongforjudge.com">andrewwongforjudge.com</a> |&nbsp;
             <a href="https://wong4judge.com">wong4judge.com</a> |&nbsp;
-            <a href="https://wongforjudge.com">wongforjudge.com</a> |&nbsp;
+            <a href="https://wongforjudge.com">wongforjudge.com</a>
 
         </>
     );
